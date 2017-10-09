7681c21cd68c73a9c622b7031927aa66423cab77
Nicolò Politi <politi@ariadne.it>
2017-10-09 18:08:18 +0200

    Added DemoArrays to demo helper package

---
+++ myJDPatternProject/src/main/java/jdp/demo/whatever/arraysDemo/MainArraysDemo.java
@@ -0,0 +1,20 @@
+package jdp.demo.whatever.arraysDemo;
+
+import java.util.ArrayList;
+
+public class MainArraysDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> stringList = new ArrayList<String>();
+		stringList.add("hi");
+		stringList.add("baby");
+		stringList.add("good bye");
+		
+		for(String element : stringList){
+			System.out.println(element);
+		}
+		
+		System.out.println(stringList.get(stringList.size()));
+	}
+
+}
